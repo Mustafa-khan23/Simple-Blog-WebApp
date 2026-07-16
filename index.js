f3a90fd1fbf85204ac5b3562777260be65a94c4a
Mustafa-khan23 <mustafakhan3237@gmail.com>
2026-07-16 16:59:47 +0530

    added pages and rendering the data

--- index.js
+++ index.js
@@ -2,22 +2,33 @@ const express = require("express");
 const path = require("path");
 const PORT = 3000;
 const app = express();
+let blogContent = require("./blogs.json");
+let name;
+for (let blogs of blogContent) {
+  name = blogs.author;
+}
 
+//middlewares
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use("/images", express.static(path.join(__dirname, "public", "images")));
+  //routes
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+  app.get("/", (req, res) => {
+    res.render("landing", { name });
+  });
 
-app.post("/post", (req, res) => {
+app.post("/", (req, res) => {
   console.log(req.body);
-
-  res.send(`Welcome ${req.body.username}`);
 });
 
+app.get("/blogs", (req, res) => {
+  res.render("blogs.ejs", { blogContent });
+});
+
+//server
 app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`);
+  console.log(`Server is listening on http://localhost:${PORT}`);
 });
